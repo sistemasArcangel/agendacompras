5641f0ac99a8c1408500f7824372acc4eaf1df5e
sistemasArcangel <117796118+sistemasArcangel@users.noreply.github.com>
2022-11-13 17:54:33 -0600

    Puerto de Gateway actualizado!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -42,7 +42,7 @@ export class HomeComponent implements OnInit {
     );
   }
   buscarContactosServicio():Observable<any>{
-   return this.http.get<any>("http://localhost:8090/contacto/buscar/correo/"+this.usuario.correo).pipe(
+   return this.http.get<any>("http://localhost:8887/contacto/buscar/correo/"+this.usuario.correo).pipe(
      catchError(e=>"error")
    )
   }
